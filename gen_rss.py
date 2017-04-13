62f961ab0b812debf1747c1d13bc3a4f84c6d6f7
Paul Nijjar <pnijjar@alumni.uwaterloo.ca>
2017-04-13 05:21:17 -0400

    Generate valid RSS feed

--- gen_rss.py
+++ gen_rss.py
@@ -4,7 +4,7 @@ import config
 
 import requests
 import pytz, datetime, dateutil.parser
-import jinja2
+import jinja2, markdown, html
 
 import pprint
 
@@ -23,8 +23,24 @@ def get_rfc822_datestring (google_date):
     d = dateutil.parser.parse(google_date)
 
     # Output the proper format
-    return d.strftime("%a, %d %b %y %T %z")
+    return d.strftime("%a, %d %b %Y %T %z")
 
+
+def get_human_datestring (google_date): 
+    """ RFC 822 is ugly for humans. Use something nicer. """
+    d = dateutil.parser.parse(google_date)
+    
+    # Wed, Oct 02 2005
+    return d.strftime("%a, %b %d %Y, %I:%M %P")
+
+def get_escaped_markdown (rawtext): 
+    """ Returns escaped markdown of rawtext (which might have had 
+        stuff before.
+    """
+    md = markdown.Markdown() 
+    md_text = md.convert(rawtext)
+    esc_text = html.escape(md_text)
+    return esc_text
 
 
 # --- Intro nonsense
@@ -59,22 +75,42 @@ feed_title = cal_dict['summary']
 
 
 template_loader = jinja2.FileSystemLoader( searchpath="/" )
-template_env = jinja2.Environment( loader=template_loader )
+template_env = jinja2.Environment( 
+    loader=template_loader,
+    autoescape=True,
+    )
 template_env.filters['rfc822'] = get_rfc822_datestring
+template_env.filters['humandate'] = get_human_datestring
+template_env.filters['emarkdown'] = get_escaped_markdown
+
+
+# https://gist.github.com/glombard/7554134
+# I want this filter to insert <p> tags into the text
+md = markdown.Markdown()
+template_env.filters['markdown'] = lambda t: jinja2.Markup(md.convert(t))
+
 
 template = template_env.get_template( TEMPLATE ) 
 template_vars = { 
   "feed_title": feed_title,
   "feed_description": cal_dict['description'],
-  "feed_builddate" : time_now.strftime("%a, %d %b %y %T %z"),
+  "feed_webmaster" : config.WEBMASTER,
+  "feed_webmaster_name" : config.WEBMASTER_NAME,
+  "feed_builddate" : time_now.strftime("%a, %d %b %Y %T %z"),
   "feed_pubdate" : cal_dict['updated'],
   "feed_website" : config.WEBSITE,
+  "feed_logo_url" : config.LOGO,
+  "feed_items" : cal_dict['items'],
+  "feed_selflink" : config.FEED_LINK,
   }
 
 output_rss = template.render(template_vars)
+
+outfile = open(config.OUTFILE, "w")
+outfile.write(output_rss)
 
 
 # pprint.pprint(r)
 # print("{}".format(r.url))
 # pprint.pprint(r.json())
-pprint.pprint(output_rss)
+# pprint.pprint(output_rss)
